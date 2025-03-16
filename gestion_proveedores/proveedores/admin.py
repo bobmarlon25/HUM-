a519d0a70b2c437617769e5671b53ded40b1246c
Marlon Julian Perez Puentes <142227718+bobmarlon25@users.noreply.github.com>
2025-03-16 14:35:24 -0500

    Delete gestion_proveedores directory

--- gestion_proveedores/proveedores/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from  .models import Invoice,Customer,Supplier
-
-admin.site.register(Invoice)
-admin.site.register(Customer)
-admin.site.register(Supplier)
